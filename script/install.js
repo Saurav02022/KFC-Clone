7cf8d98a655d052e988ed4451f618907879bedbb
Suryawasnhi14sagar@gmail.com <105620686+sagar-surya@users.noreply.github.com>
2022-10-02 11:24:12 +0530

    saurav ne bols ths krne ko

---
+++ script/install.js
@@ -0,0 +1,8 @@
+import header from "../components/navbar.js";
+
+let display = document.getElementById("ps_header")
+display.innerHTML = header()
+
+import { footer } from "../components/footer.js";
+let saurav = document.getElementById("ps_footer")
+saurav.innerHTML = footer()
